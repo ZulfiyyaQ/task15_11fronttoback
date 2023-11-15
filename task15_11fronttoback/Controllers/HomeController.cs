eee40a5dd89bdd33c204d2dffff83275106b3e36
ZulfiyyaQ <tu7u71zbq@code.edu.az>
2023-11-15 23:40:43 +0400

    added new task

---
+++ task15_11fronttoback/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace task15_11fronttoback.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
